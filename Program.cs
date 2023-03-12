9c01eec9dc115256a3743744352e9a2fd7c2f32b
Ram <rmailtat777@gmail.com>
2023-03-12 13:08:31 +0300

    description of methods for creating and outputting an array

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string[] GetArray(string text)
+{
+    System.Console.WriteLine(text);
+    string[] array = Console.ReadLine()!.Split();
+    return array;
+}
+
+
+void PrintArray(string[] arr)
+{
+    System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
+}
+
