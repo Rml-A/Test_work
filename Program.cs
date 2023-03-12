f49be4312ec28c143ebcdc73a86c6957c2dc890e
Ram <rmailtat777@gmail.com>
2023-03-12 13:11:46 +0300

    creating a method for calculating the size of a new array

--- Program.cs
+++ Program.cs
@@ -11,3 +11,16 @@ void PrintArray(string[] arr)
     System.Console.WriteLine("[" + string.Join(", ", arr) + "]");
 }
 
+void SizeNewArray(string[] mass, out int size)
+{
+    size = mass.Length;
+    for (int j = 0; j < mass.Length; j++)
+    {
+        if (mass[j].Length > 3)
+        {
+            size--;
+        }
+    }
+}
+
+
